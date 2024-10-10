b21061a432381ef8298e57aca906b636ffcb4174
Jomagene <semjomagene@gmail.com>
2024-10-10 08:23:01 +0200

    Exercise 1: Looping a Triangle

---
+++ Chap1.Ex.js
@@ -0,0 +1,15 @@
+// 1. LOOPING A TRIANGLE
+
+// Solution 1
+/*let line = "";
+for (let i = 0; i < 7; i++) {
+  line += "#";
+  console.log(line);
+}*/
+
+// Solution 2
+/*for (let i = 1; i <= 7; i++) {
+  console.log("#".repeat(i));
+}*/
+
+// ===============================================================
