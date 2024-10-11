26ec5e34a3b1f37a10596d197289113a7000d583
Jomagene <semjomagene@gmail.com>
2024-10-11 11:58:02 +0200

    Add a reverse array solution

--- chap3.Functions.js
+++ chap3.Functions.js
@@ -3,30 +3,30 @@
 // 1. MINIMUM
 
 // Solution 1
-// function minimum(num1, num2) {
-//   if (num1 < num2) {
-//     return num1;
-//   } else {
-//     return num2;
-//   }
-// }
+function minimum(num1, num2) {
+  if (num1 < num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
 
 // Solution 2 using ternary
-// function minimum(num1, num2) {
-//   return num1 < num2 ? num1 : num2;
-// }
+function minimum(num1, num2) {
+  return num1 < num2 ? num1 : num2;
+}
 
 // ===============================================================
 
 // 2. RECURSION
 
 // Solution
-// function isEven(number) {
-//   number = Math.abs(number);
-//   if (number == 0) return "even";
-//   else if (number == 1) return "odd";
-//   else return isEven(number - 2);
-// }
+function isEven(number) {
+  number = Math.abs(number);
+  if (number == 0) return "even";
+  else if (number == 1) return "odd";
+  else return isEven(number - 2);
+}
 
 // console.log(isEven(-1));
 
@@ -35,21 +35,21 @@
 //3 BEAN COUNTING
 
 // Solution
-// function countBs(string) {
-//   let countB = 0;
-//   for (let i = 0; i < string.length; i++) {
-//     if (string[i] == "B") countB++;
-//   }
-//   return countB;
-// }
+function countBs(string) {
+  let countB = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] == "B") countB++;
+  }
+  return countB;
+}
 
-// function countChar(string, char) {
-//   let count = 0;
-//   for (let i = 0; i < string.length; i++) {
-//     if (string[i] == char) count++;
-//   }
-//   return count;
-// }
+function countChar(string, char) {
+  let count = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] == char) count++;
+  }
+  return count;
+}
 
 // Solution 2
 function countChar(string, char) {
--- chap4/chap4.dataStructures.js
+++ chap4/chap4.dataStructures.js
@@ -3,27 +3,27 @@
 // 1. THE SUM OF A RANGE
 // Solution 1
 
-// function range(start, end, step = 1) {
-//   let startToEnd = [];
-//   if (step < 0)
-//     for (let i = start; i >= end; i += step) {
-//       startToEnd.push(i);
-//     }
-//   else
-//     for (let i = start; i <= end; i += step) {
-//       startToEnd.push(i);
-//     }
+function range(start, end, step = 1) {
+  let startToEnd = [];
+  if (step < 0)
+    for (let i = start; i >= end; i += step) {
+      startToEnd.push(i);
+    }
+  else
+    for (let i = start; i <= end; i += step) {
+      startToEnd.push(i);
+    }
 
-//   return startToEnd;
-// }
+  return startToEnd;
+}
 
-// function sum(range) {
-//   let result = 0;
-//   for (let element of range) {
-//     result += element;
-//   }
-//   return result;
-// }
+function sum(range) {
+  let result = 0;
+  for (let element of range) {
+    result += element;
+  }
+  return result;
+}
 
 // Solution 2
 function range(start, end, step = 1) {
@@ -37,4 +37,26 @@ function range(start, end, step = 1) {
 
 function sum(range) {
   return range.reduce((prev, cur) => prev + cur);
+}
+
+// ===============================================================
+
+// 2. REVERSING AN ARRAY
+// Solution 1
+
+function reverseArray(array) {
+  let reversedArray = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    reversedArray.push(array[i]);
+  }
+  return reversedArray;
+}
+
+function reverseArrayInPlace(array) {
+  for (let i = 0; i < array.length / 2; i++) {
+    let temporal = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = temporal;
+  }
+  return array;
 }
