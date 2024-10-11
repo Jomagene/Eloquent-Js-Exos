2762018dcc5bac8c99a409c4f2961307950c4da6
Jomagene <semjomagene@gmail.com>
2024-10-11 11:31:21 +0200

    Propose a shorter solution to the first exercise of chap 4

--- chap4/chap4.dataStructures.js
+++ chap4/chap4.dataStructures.js
@@ -1,1 +1,40 @@
+// These are solutions to the data structure chapter  exercises from the book Eloquent Javascript 4th version
 
+// 1. THE SUM OF A RANGE
+// Solution 1
+
+// function range(start, end, step = 1) {
+//   let startToEnd = [];
+//   if (step < 0)
+//     for (let i = start; i >= end; i += step) {
+//       startToEnd.push(i);
+//     }
+//   else
+//     for (let i = start; i <= end; i += step) {
+//       startToEnd.push(i);
+//     }
+
+//   return startToEnd;
+// }
+
+// function sum(range) {
+//   let result = 0;
+//   for (let element of range) {
+//     result += element;
+//   }
+//   return result;
+// }
+
+// Solution 2
+function range(start, end, step = 1) {
+  let startToEnd = [];
+  let negative = step < 0;
+  for (let i = start; negative ? i >= end : i <= end; i += step)
+    startToEnd.push(i);
+
+  return startToEnd;
+}
+
+function sum(range) {
+  return range.reduce((prev, cur) => prev + cur);
+}
